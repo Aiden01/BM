e3406af301c21afd4d044da5fdc54726c442c680
Elian Gutierrez <eliangu3600103@gmail.com>
2018-10-14 11:08:05 -0400

    chore(indexeddb): Show error when indexedDB is not available

--- src/db/index.js
+++ src/db/index.js
@@ -2,6 +2,24 @@ import Dexie from 'dexie'
 
 const db = new Dexie('bm')
 
+window.addEventListener('unhandledrejection', (ev) => {
+    alert(`An error occurred: ${ev.reason}`)
+}) 
+
+
+db.open()
+    .then(() => console.log('Database openned'))
+    .catch(() => indexedDBNotAvailable())
+
+
+if(!window.indexedDB) {
+    indexedDBNotAvailable()
+}
+
+function indexedDBNotAvailable() {
+    alert('Indexed DB is not supported by your browser. If you are running in incognito mode, please use the normal mode.')
+}
+
 db.version(1).stores({
     bookmarks: '++id, description, link'
 })
